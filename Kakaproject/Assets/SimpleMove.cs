2ca676aabbbc959451de7880ec8c2e07183640ba
Joakim Isaksson <Joakim Isaksson>
2016-11-25 22:48:18 +0200

    first characters

---
+++ Kakaproject/Assets/SimpleMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMove : MonoBehaviour {
+
+    public float Speed;
+
+    Rigidbody2D rb;
+    Unit unit;
+    
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        unit = GetComponent<Unit>();
+    }
+    
+    void Update()
+    {
+        if (unit.State == UnitState.Alive)
+            rb.MovePosition(new Vector2(transform.position.x, transform.position.y + Speed * Time.deltaTime));
+    }
+}
---
+++ Kakaproject/Assets/Unit.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+
+    public int MinDamage;
+    public int MaxDamage;
+    public int MaxHitPoints;
+
+    public GameObject ObjAlive;
+    public GameObject ObjDead;
+    public GameObject ObjExpired;
+
+    [HideInInspector]
+    public UnitState State;
+
+    int hitPoints;
+    SpriteRenderer spriteRenderer;
+    
+	void Start () {
+        SetState(UnitState.Alive);
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+    
+    void Update()
+    {
+        spriteRenderer.sortingOrder = (int)(transform.position.y * 10000);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (State != UnitState.Alive) return;
+
+        hitPoints -= amount;
+        if (hitPoints < 1)
+        {
+            SetState(UnitState.Dead);
+            StartCoroutine(Expire());
+        }
+    }
+
+    IEnumerator Expire()
+    {
+        yield return new WaitForSeconds(10f);
+        SetState(UnitState.Expired);
+    }
+
+    void SetState(UnitState newState)
+    {
+        State = newState;
+        switch (State)
+        {
+            case UnitState.Alive:
+                ObjAlive.SetActive(true);
+                ObjDead.SetActive(false);
+                ObjExpired.SetActive(false);
+                break;
+            case UnitState.Dead:
+                ObjAlive.SetActive(false);
+                ObjDead.SetActive(true);
+                ObjExpired.SetActive(false);
+                break;
+            case UnitState.Expired:
+                ObjAlive.SetActive(false);
+                ObjDead.SetActive(false);
+                ObjExpired.SetActive(true);
+                break;
+        }
+    }
+}
